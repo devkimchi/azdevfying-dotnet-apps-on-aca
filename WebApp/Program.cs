a2034709a419a5b65b6e1b44398056779646cb5c
Justin Yoo <justin.yoojh@gmail.com>
2024-09-03 13:09:28 +0900

    Update apps

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,4 @@
+using WebApp.Clients;
 using WebApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<IApiAppClient, ApiAppClient>((sp, client) =>
+{
+#if DEBUG
+    client.BaseAddress = new Uri("https://localhost:5051");
+#elif RELEASE
+    var config = sp.GetService<IConfiguration>();
+    client.BaseAddress = new Uri(config!["API_ENDPOINT_URL"]!);
+#endif
+});
+
+builder.Services.AddHttpClient<IFuncAppClient, FuncAppClient>((sp, client) =>
+{
+#if DEBUG
+    client.BaseAddress = new Uri("http://localhost:7071");
+#elif RELEASE
+    var config = sp.GetService<IConfiguration>();
+    client.BaseAddress = new Uri(config!["FUNC_ENDPOINT_URL"]!);
+#endif
+});
 
 var app = builder.Build();
 
